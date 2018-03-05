e62b1842e361006c3265ae89fa16114213b6e092
pan <panbanglanfeng@gmail.com>
2018-03-04 23:30:52 -0500

    draw cor by converting to numeric cols

--- main.R
+++ main.R
@@ -1,14 +1,28 @@
 library(tidyverse)
 library(caret)
+library(corrplot)
 
-training<-read_csv('train.csv')
-testing<-read_csv('test.csv')
+training <- read_csv('train.csv')
+testing <- read_csv('test.csv')
 
-full<-bind_rows(training,testing)
+full <- bind_rows(training, testing)
 summary(full)
 head(full)
 
-inTrain = createDataPartition(training$Survived, p = 3/4)[[1]]
+list <-
+  c('Sex', 'Ticket', 'Survived', 'Embarked', 'Pclass', 'Cabin')
+
+full[, list] <- lapply(full[, list], function(x) {
+  y <- as.factor(x)
+  y <- as.numeric(y)
+  y
+})
+
+str(full)
+
+inTrain = createDataPartition(training$Survived, p = 3 / 4)[[1]]
 training_set = training[inTrain,]
 testing_set = training[-inTrain,]
 
+numeric_cols = na.omit(full[,list])
+corrplot(cor(numeric_cols), method = "circle")
