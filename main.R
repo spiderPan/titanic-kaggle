8df3ee73be5b48ac15c73850cceeaea53358c47e
Owen Ouyang <oouyang@J2PMR3212.wattsworth.com>
2018-03-11 00:35:11 -0500

    march11

--- main.R
+++ main.R
@@ -7,16 +7,24 @@ library(randomForest)
 training <- read_csv('train.csv')
 testing <- read_csv('test.csv')
 
+str(training)
+str(testing)
+
+
 full <- bind_rows(training, testing)
 summary(full)
 head(full)
 
-NameSet = str_split_fixed(full$Name, ',', 2)
-FirstNameSet = str_split_fixed(str_trim(NameSet[, 2]), "[.]",2)
+
+
+(NameSet = str_split_fixed(full$Name, ',', 2))
+(FirstNameSet = str_split_fixed(str_trim(NameSet[, 2]), "[.]",2))
 
 full$LastName = NameSet[, 1]
-full$Title = str_trim(FirstNameSet[,1])
-full$FirstName = str_trim(FirstNameSet[,2])
+(full$Title = str_trim(FirstNameSet[,1]))
+(full$FirstName = str_trim(FirstNameSet[,2]))
+
+str(full)
 
 full$Family = full$SibSp + full$Parch + 1
 full$IsFamily = paste(full$Family, full$LastName)
@@ -34,7 +42,43 @@ full[, list] <- lapply(full[, list], function(x) {
   y <- as.numeric(y)
   y
 })
-str(full)
+
+sum(is.na(full$Title))
+
+full$Title %>% table()
+
+full$Survived<-as.factor(full$Survived)
+full$Title<-as.factor(full$Title)
+
+full[1:nrow(training),] %>% ggplot(aes(Survived, fill=Survived))+geom_bar()+facet_grid(.~Title)
+
+full$Title %>% table()
+full[1:nrow(training),] %>% filter(Title=='Col') %>% nrow()
+
+full1 <- full
+
+full$Title <- as.character(full$Title)
+
+full1$Title[full1$Title %in% c("Capt",'Col','Don','Dona','Jonkheer','Lady','Major','Mlle','Mme',
+                             'Ms','Rev','Dr','Sir','the Countess')]<-"Rare"
+
+str(full1)
+
+summary(full1)
+
+full[is.na(full1$Embarked),] %>% str()
+
+full[is.na(full1$Fare),] %>% str()
+
+full1 %>% filter(Cabin==29) %>% str()
+
+full1 %>% select(Ticket,Embarked) %>% table()
+full1 %>% select(Ticket,Embarked) %>% unique() %>% 
+  ggplot(aes(Ticket,Embarked))+geom_col()
+
+full1 %>% select(Ticket,Embarked) %>% unique() %>% 
+  ggplot(aes(Ticket,Embarked))+geom_point()
+
 
 inTrain = createDataPartition(training$Survived, p = 3 / 4)[[1]]
 training_set = training[inTrain,]
@@ -42,6 +86,7 @@ testing_set = training[-inTrain,]
 
 numeric_cols = na.omit(full[, list])
 corrplot(cor(numeric_cols), method = "circle")
+
 
 
 
