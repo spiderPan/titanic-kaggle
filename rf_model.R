e399f0035ff6a72de4d06b4560d5927b310c1baf
couyang24 <30837746+couyang24@users.noreply.github.com>
2018-03-15 15:52:35 -0400

    visualization functions for single variables

--- rf_model.R
+++ rf_model.R
@@ -11,7 +11,6 @@ train<-read_csv("data/train.csv")
 test<-read_csv("data/test.csv")
 
 full<-bind_rows(train,test)
-rm(train,test)
 
 # Check data
 head(full)
@@ -19,32 +18,79 @@ str(full)
 summary(full)
 sapply(full,function(x) sum(is.na(x)))
 
-# EDA
+# Toolbox:
+
+## Visualize Discrete Single Variables
+vis_bar<-function(dataset,variable){
+  
+  (g1<-dataset %>% filter(!is.na(Survived)) %>% 
+     ggplot(aes(dataset[[variable]],fill=as.factor(Survived)))+geom_bar(position = "stack")+
+     theme_economist()+labs(fill="Survived",x=variable))
+  
+  (g2<-dataset %>% filter(!is.na(Survived)) %>% 
+      ggplot(aes(dataset[[variable]],fill=as.factor(Survived)))+geom_bar(position = "fill")+
+      theme_economist()+labs(fill="Survived",x=variable))
+  
+  (g3<-dataset %>% filter(!is.na(Survived)) %>% 
+      ggplot(aes(dataset[[variable]],fill=as.factor(Survived)))+geom_bar(position = "dodge")+
+      theme_economist()+labs(fill="Survived",x=variable))
+  
+  (Title<- ggplot(data=data.frame(x=0,y=0))+geom_point(aes(x=x,y=y),size=-1)+
+      labs(x="",y="")+
+      annotate('text', x = 0, y = 0, label = paste0(variable," vs Survived \n multi_views"),size=8)+
+      theme(axis.text.x = element_blank(),axis.text.y = element_blank(),axis.ticks = element_blank(),
+            panel.background = element_blank(),
+            panel.border = element_blank(),
+            panel.grid = element_blank(),plot.margin = unit(c(0,0,-1,-1),"cm")))
+  
+  grid.arrange(Title,g1,g2,g3,layout_matrix=t(matrix(c(1,2,
+                                                       3,4),nrow=2)))
+}
+
+## Visualize Continuous Single Variables
+vis_hist<-function(dataset,variable,binwidths=5){
+  
+  (g1<-dataset %>% filter(!is.na(Survived)) %>% 
+     ggplot(aes(dataset[[variable]],fill=as.factor(Survived)))+geom_histogram(position = "stack",binwidth = binwidths)+
+     theme_economist()+labs(fill="Survived",x=variable))
+  
+  (g2<-dataset %>% filter(!is.na(Survived)) %>% 
+      ggplot(aes(dataset[[variable]],fill=as.factor(Survived)))+geom_histogram(position = "fill",binwidth = binwidths)+
+      theme_economist()+labs(fill="Survived",x=variable))
+  
+  (g3<-dataset %>% filter(!is.na(Survived)) %>% 
+      ggplot(aes(dataset[[variable]],fill=as.factor(Survived)))+geom_histogram(position = "dodge",binwidth = binwidths)+
+      theme_economist()+labs(fill="Survived",x=variable))
+  
+  (Title<- ggplot(data=data.frame(x=0,y=0))+geom_point(aes(x=x,y=y),size=-1)+
+      labs(x="",y="")+
+      annotate('text', x = 0, y = 0, label = paste0(variable," vs Survived \n multi_views"),size=8)+
+      theme(axis.text.x = element_blank(),axis.text.y = element_blank(),axis.ticks = element_blank(),
+            panel.background = element_blank(),
+            panel.border = element_blank(),
+            panel.grid = element_blank(),plot.margin = unit(c(0,0,-1,-1),"cm")))
+  
+  grid.arrange(Title,g1,g2,g3,layout_matrix=t(matrix(c(1,2,
+                                                       3,4),nrow=2)))
+}
+
+
+# EDA - part 1 direct one variable
 ## Age
-
-(age_g1<-full %>% filter(!is.na(Survived)) %>% 
-    ggplot(aes(Age,fill=as.factor(Survived)))+geom_histogram(position = "stack")+
-    theme_economist()+labs(fill="Survived"))
-
-(age_g2<-full %>% filter(!is.na(Survived)) %>% 
-    ggplot(aes(Age,fill=as.factor(Survived)))+geom_histogram(position = "fill")+
-    theme_economist()+labs(fill="Survived"))
-
-(age_g3<-full %>% filter(!is.na(Survived)) %>% 
-    ggplot(aes(Age,fill=as.factor(Survived)))+geom_histogram(position = "dodge")+
-    theme_economist()+labs(fill="Survived"))
-
-(Title<- ggplot(data=data.frame(x=0,y=0))+geom_point(aes(x=x,y=y),size=-1)+
-  labs(x="",y="")+
-  annotate('text', x = 0, y = 0, label = "Age vs Survived \n multi_views",size=10)+
-  theme(axis.text.x = element_blank(),axis.text.y = element_blank(),axis.ticks = element_blank(),
-        panel.background = element_blank(),
-        panel.border = element_blank(),
-        panel.grid = element_blank(),plot.margin = unit(c(0,0,-1,-1),"cm")))
-
-grid.arrange(Title,age_g1,age_g2,age_g3,layout_matrix=t(matrix(c(1,2,
-                                                                 3,4),nrow=2)))
+vis_hist(train,"Age",5)
 
 ## Sex
+vis_bar(train,"Sex")
 
+## Pclass
+vis_bar(train,"Pclass")
+
+# SibSp
+vis_bar(train,"SibSp")
+
+# Parch
+vis_bar(train, "Parch")
+
+# Embarked
+vis_bar(train, "Embarked")
 
